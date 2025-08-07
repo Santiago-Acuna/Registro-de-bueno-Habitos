5187a13ebc901e0de35f6a11dbc4d5c4f7722357
Santiago Gabriel Acuña <santiagoacu1990@gmail.com>
2025-08-07 13:35:05 -0300

    options.validationSchema.validate is not a function nest error solved

--- server/src/infrastructure/config/config.validation.ts
+++ server/src/infrastructure/config/config.validation.ts
@@ -1,28 +1,22 @@
-import { IsString, IsNotEmpty, IsInt, IsIn, IsOptional  } from '@nestjs/class-validator';
+// src/config.validation.ts
+import * as Joi from 'joi';
 
+export const configValidation = Joi.object({
+  NODE_ENV: Joi.string().valid('development', 'production', 'test').required(),
 
-export class configValidation {
-  @IsString()
-  @IsNotEmpty()
-  NODE_ENV!: string;
-  @IsInt()
-  @IsOptional()
-  PORT:number = 3000;
-  @IsString()
-  @IsOptional()
-  API_VERSION: string = 'v1';
-  @IsString()
-  @IsNotEmpty()
-  DATABASE_URL!: string;
-  @IsString()
-  @IsOptional()
-  CORS_ORIGIN: string= "http://localhost:5173";
-  @IsInt()
-  RATE_LIMIT_MAX: number = 100;
-  @IsInt()
-  @IsOptional()
-  RATE_LIMIT_WINDOW: number = 60000;
-  @IsIn(['error', 'warn', 'log', 'debug', 'verbose'])
-  @IsOptional()
-  LOG_LEVEL: string = 'log';
-}
+  PORT: Joi.number().default(3000),
+
+  API_VERSION: Joi.string().default('v1'),
+
+  DATABASE_URL: Joi.string().required(),
+
+  CORS_ORIGIN: Joi.string().default('http://localhost:5173'),
+
+  RATE_LIMIT_MAX: Joi.number().default(100),
+
+  RATE_LIMIT_WINDOW: Joi.number().default(60000),
+
+  LOG_LEVEL: Joi.string()
+    .valid('error', 'warn', 'log', 'debug', 'verbose')
+    .default('log'),
+});
