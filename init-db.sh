63eaab519dd2c5085e30fe7df47ded55f8453250
Santiago Gabriel Acuña <santiagoacu1990@gmail.com>
2025-08-25 19:19:05 -0300

    DB config change for flyway

--- init-db.sh
+++ init-db.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 set -e
-echo "DATABASE_USER=$DATABASE_USER"
 echo "DATABASE_NAME=$DATABASE_NAME"
 echo "DATABASE_PASSWORD=$DATABASE_PASSWORD"
 
-# Create the role if it doesn't exist
-psql -U "$POSTGRES_USER" -d postgres -tc "SELECT 1 FROM pg_roles WHERE rolname = '$DATABASE_USER'" | grep -q 1 || \
-psql -U "$POSTGRES_USER" -d postgres -c "CREATE ROLE $DATABASE_USER WITH LOGIN PASSWORD '$DATABASE_PASSWORD';"
+psql -U "$POSTGRES_USER" -d postgres -tc "SELECT 1 FROM pg_roles WHERE rolname = '$FLYWAY_USER'" | grep -q 1 || \
+psql -U "$POSTGRES_USER" -d postgres -c "CREATE USER $FLYWAY_USER WITH LOGIN PASSWORD '$DATABASE_PASSWORD';"
+
+psql -U "$POSTGRES_USER" -d postgres -tc "SELECT 1 FROM pg_roles WHERE rolname = '$PRISMA_USER'" | grep -q 1 || \
+psql -U "$POSTGRES_USER" -d postgres -c "CREATE USER $PRISMA_USER WITH LOGIN PASSWORD '$DATABASE_PASSWORD';"
 
 # Create the main database if it doesn't exist
 psql -U "$POSTGRES_USER" -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = '$DATABASE_NAME'" | grep -q 1 || \
 psql -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE $DATABASE_NAME;"
 
-# Create the shadow database if it doesn't exist
-psql -U "$POSTGRES_USER" -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'shadow'" | grep -q 1 || \
-psql -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE shadow;"
+# Change ownership
+psql -U "$POSTGRES_USER" -d postgres -c "ALTER DATABASE \"$DATABASE_NAME\" OWNER TO $FLYWAY_USER;"
+psql -U "$POSTGRES_USER" -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"$DATABASE_NAME\" TO $FLYWAY_USER;"
 
-# Change ownership of both databases to prisma_user
-psql -U "$POSTGRES_USER" -d postgres -c "ALTER DATABASE \"$DATABASE_NAME\" OWNER TO $DATABASE_USER;"
-psql -U "$POSTGRES_USER" -d postgres -c "ALTER DATABASE shadow OWNER TO $DATABASE_USER;"
+psql -U "$POSTGRES_USER" -d postgres -c "GRANT CONNECT ON DATABASE $DATABASE_NAME TO $PRISMA_USER;"
+psql -U "$POSTGRES_USER" -d postgres -c "GRANT USAGE ON SCHEMA public TO $PRISMA_USER;"
+psql -U "$POSTGRES_USER" -d postgres -c "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO $PRISMA_USER;"
+psql -U "$POSTGRES_USER" -d postgres -c "GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO $PRISMA_USER;"
 
-# Enable extensions in both databases
-for DB in "$DATABASE_NAME" shadow; do
-  psql -U "$POSTGRES_USER" -d "$DB" -c "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"
+# Enable required extensions
+for DB in "$DATABASE_NAME"; do
   psql -U "$POSTGRES_USER" -d "$DB" -c "CREATE EXTENSION IF NOT EXISTS \"pg_stat_statements\";"
 done
