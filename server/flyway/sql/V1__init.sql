9f8d35169fc91f2a89c505a23272d449f26a3591
Santiago Gabriel Acuña <santiagoacu1990@gmail.com>
2025-08-29 20:06:54 -0300

    flayway init

---
+++ server/flyway/sql/V1__init.sql
@@ -0,0 +1,41 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict THKU7y5QW4zuRwMP3zjBRqUMZ5JPfWLDoQdQ5ICGswcSTEIgVkufipqYvXDhZKM
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.6
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: pg_stat_statements; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pg_stat_statements; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION pg_stat_statements IS 'track planning and execution statistics of all SQL statements executed';
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict THKU7y5QW4zuRwMP3zjBRqUMZ5JPfWLDoQdQ5ICGswcSTEIgVkufipqYvXDhZKM
+
