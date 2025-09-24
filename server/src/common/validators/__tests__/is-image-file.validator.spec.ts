ffeb3f1c9d010bd4a769a3301a476a8f4e59ea80
Santiago Gabriel Acuña <santiagoacu1990@gmail.com>
2025-09-24 09:56:25 -0300

    test(validator): add IsImageFileConstraint test verification

---
+++ server/src/common/validators/__tests__/is-image-file.validator.spec.ts
@@ -0,0 +1,7 @@
+import { IsImageFileConstraint } from '../is-image-file.validator';
+
+describe('IsImageFileConstraint', () => {
+  it('should be defined', () => {
+    expect(IsImageFileConstraint).toBeDefined();
+  });
+});
