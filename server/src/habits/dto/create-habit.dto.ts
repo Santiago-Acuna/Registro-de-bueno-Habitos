ce43506244065460d7ea0724155bd61397d73263
Santiago Gabriel Acuña <santiagoacu1990@gmail.com>
2025-10-02 18:50:31 -0300

    refactor: update CreateHabitDto to use logo property (API compatibility)

--- server/src/habits/dto/create-habit.dto.ts
+++ server/src/habits/dto/create-habit.dto.ts
@@ -1,5 +1,5 @@
-import { IsString, IsEnum, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEnum, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 import { HabitComplexity } from '../../domain/shared/types/common';
 
