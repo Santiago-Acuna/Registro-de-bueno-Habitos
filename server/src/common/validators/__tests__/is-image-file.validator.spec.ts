3113f74122e70ad545800b413831cb05d1456df2
Santiago Gabriel Acuña <santiagoacu1990@gmail.com>
2025-10-03 08:40:52 -0300

    chore: remove empty is-image-file.validator.spec.ts test file

--- server/src/common/validators/__tests__/is-image-file.validator.spec.ts
+++
@@ -1,7 +0,0 @@
-import { IsImageFileConstraint } from '../is-image-file.validator';
-
-describe('IsImageFileConstraint', () => {
-  it('should be defined', () => {
-    expect(IsImageFileConstraint).toBeDefined();
-  });
-});
