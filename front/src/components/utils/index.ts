fe712d0234eaa4f12f903dd17cfdbaae4d161013
Santiago Gabriel Acuña <santiagoacu1990@gmail.com>
2026-03-01 09:22:09 -0300

    feat(utils): export TerminatorForm from utils barrel export
    Add TerminatorForm to the utils components barrel export for consistent import patterns across the application.

--- front/src/components/utils/index.ts
+++ front/src/components/utils/index.ts
@@ -1,2 +1,3 @@
 // Barrel export for Utils components
 export { default as TerminatorButton } from "./terminator-button/terminator-button";
+export { default as TerminatorForm } from "./terminator-form/terminator-form";
